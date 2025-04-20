6640a1756e738c97bf4dcbc8b05f9204a5102d39
Dmitry <pokemon2708@yandex.ru>
2025-04-20 21:14:23 +0300

    Карты и знакомство

--- upload.py
+++ upload.py
@@ -57,11 +57,8 @@ def handle_dialog(res, req):
         if first_name is None:
             res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
         else:
-            sessionStorage[user_id]['first_name'] = first_name
-            # создаём пустой массив, в который будем записывать города, которые пользователь уже отгадал
+            sessionStorage[user_id]['first_name'] = first_name.capitalize()
             sessionStorage[user_id]['guessed_cities'] = []
-            # как видно из предыдущего навыка, сюда мы попали, потому что пользователь написал своем имя.
-            # Предлагаем ему сыграть и два варианта ответа "Да" и "Нет".
             res['response']['text'] = f'Приятно познакомиться, {first_name.title()}. Я Алиса. Отгадаешь город по фото?'
             res['response']['buttons'] = [
                 {
@@ -81,7 +78,7 @@ def handle_dialog(res, req):
         if not sessionStorage[user_id]['game_started']:
             if 'да' in req['request']['nlu']['tokens']:
                 if len(sessionStorage[user_id]['guessed_cities']) == 3:
-                    res['response']['text'] = 'Ты отгадал все города!'
+                    res['response']['text'] = sessionStorage[user_id]['first_name'] + ', ты отгадал все города!'
                     res['response']['end_session'] = True
                 else:
                     sessionStorage[user_id]['game_started'] = True
@@ -92,7 +89,7 @@ def handle_dialog(res, req):
                 res['response']['text'] = 'Ну и ладно!'
                 res['response']['end_session'] = True
             else:
-                res['response']['text'] = 'Не поняла ответа! Так да или нет?'
+                res['response']['text'] = sessionStorage[user_id]['first_name'] + ', я не поняла твоего ответа! Так да или нет?'
                 res['response']['buttons'] = [
                     {
                         'title': 'Да',
@@ -117,7 +114,6 @@ def play_game(res, req):
     if attempt == 1:
         # если попытка первая, то случайным образом выбираем город для гадания
         city = random.choice(list(cities))
-        # выбираем его до тех пор пока не выбираем город, которого нет в sessionStorage[user_id]['guessed_cities']
         while city in sessionStorage[user_id]['guessed_cities']:
             city = random.choice(list(cities))
         # записываем город в информацию о пользователе
@@ -126,7 +122,7 @@ def play_game(res, req):
         # добавляем в ответ картинку
         res['response']['card'] = {}
         res['response']['card']['type'] = 'BigImage'
-        res['response']['card']['title'] = 'Что это за город?'
+        res['response']['card']['title'] = sessionStorage[user_id]['first_name'] + ', что это за город?'
         res['response']['card']['image_id'] = cities[city][1][attempt - 1]
         res['response']['text'] = 'Тогда сыграем!'
     else:
@@ -188,10 +184,8 @@ def play_game(res, req):
 
         if get_city(req) == city:
             # если отгодали
-            res['response']['text'] = 'Правильно! А в какой стране этот город?'
+            res['response']['text'] = sessionStorage[user_id]['first_name'] + ', правильно! А в какой стране этот город?'
             sessionStorage[user_id]['is_city'] = False
-            # sessionStorage[user_id]['guessed_cities'].append(city)
-            # sessionStorage[user_id]['game_started'] = False
             res['response']['buttons'] = [
                 {
                     "hide": True,
@@ -205,12 +199,8 @@ def play_game(res, req):
             ]
             return
         else:
-            # если нет
             if attempt == 3:
                 # если попытка третья, то значит, что все картинки мы показали.
-                # В этом случае говорим ответ пользователю,
-                # добавляем город к sessionStorage[user_id]['guessed_cities'] и отправляем его на второй круг.
-                # Обратите внимание на этот шаг на схеме.
                 res['response']['text'] = f'Вы пытались. Это {city.title()}. Сыграем ещё?'
                 sessionStorage[user_id]['game_started'] = False
                 sessionStorage[user_id]['guessed_cities'].append(city)
@@ -238,7 +228,7 @@ def play_game(res, req):
                 # иначе показываем следующую картинку
                 res['response']['card'] = {}
                 res['response']['card']['type'] = 'BigImage'
-                res['response']['card']['title'] = 'Неправильно. Вот тебе дополнительное фото'
+                res['response']['card']['title'] = f'Неправильно {sessionStorage[user_id]['first_name']}. Вот тебе дополнительное фото'
                 res['response']['card']['image_id'] = cities[city][1][attempt - 1]
                 res['response']['text'] = 'А вот и не угадал!'
     # увеличиваем номер попытки доля следующего шага
@@ -260,30 +250,17 @@ def get_first_name(req):
 def get_geo_info(city_name, type_info):
     if type_info == 'coordinates':
         try:
-            # url, по которому доступно API Яндекс.Карт
             url = "https://geocode-maps.yandex.ru/1.x/"
-            # параметры запроса
             params = {
                 "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
-                # город, координаты которого мы ищем
                 'geocode': city_name,
-                # формат ответа от сервера, в данном случае JSON
                 'format': 'json'
             }
-            # отправляем запрос
             response = requests.get(url, params)
-            # получаем JSON ответа
             json = response.json()
-            # получаем координаты города
-            # (там написаны долгота(longitude), широта(latitude) через пробел)
-            # посмотреть подробное описание JSON-ответа можно
-            # в документации по адресу https://tech.yandex.ru/maps/geocoder/
             coordinates_str = json['response']['GeoObjectCollection'][
                 'featureMember'][0]['GeoObject']['Point']['pos']
-            # Превращаем string в список, так как
-            # точка - это пара двух чисел - координат
             long, lat = map(float, coordinates_str.split())
-            # Вернем ответ
             return long, lat
         except Exception as e:
             return e
@@ -297,7 +274,6 @@ def get_geo_info(city_name, type_info):
                 'format': 'json'
             }
             data = requests.get(url, params).json()
-            # все отличие тут, мы получаем имя страны
             return data['response']['GeoObjectCollection'][
                 'featureMember'][0]['GeoObject']['metaDataProperty'][
                 'GeocoderMetaData']['AddressDetails']['Country']['CountryName']
